c993ae59349f94b7e8e512ef1d04943d23b21f85
Nolan Glore <nglore@microsoft.com>
2020-11-11 20:48:03 -0800

    Add asp.net framework hello app

---
+++ AspnetframeworkHello/HelloHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace AspnetframeworkHello
+{
+    using System.Diagnostics;
+    using System.Web;
+
+    public class HelloHandler : IHttpHandler
+    {
+        private readonly int pid = Process.GetCurrentProcess().Id;
+
+        public bool IsReusable => true;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write($"Hello world from ASP.NET Framework ({pid})");
+        }
+    }
+}
